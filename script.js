5cdfea3fcc406ac0e9251b99d898b1cd654e4c1e
Gastón Gómez <gaston.gomez.1998@gmail.com>
2021-09-17 15:52:05 -0300

    Added win function and reset function

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let square = document.querySelectorAll('.square')
 
 const PLAYER1 = 'x'
 const PLAYER2 = 'o'
-let player1Turn = true;
-let grid = [[],[],[]]
+let player1Turn
+let grid
 
 function assignContent(arg){
     if(player1Turn){
@@ -46,29 +46,64 @@ function assignContent(arg){
     }
 }
 
-square.forEach(element => {
-    let numOfClicks = 0
-    switch(element.id){
-        case '1':
-        case '2':
-        case '3':
-            grid[0].push(undefined)
-            break;
-        case '4':
-        case '5':
-        case '6':
-            grid[1].push(undefined)
-            break;
-        default:
-            grid[2].push(undefined)
-            break;
-    }
-    element.addEventListener('click', (e) => {
-            numOfClicks+=1;
-            if(numOfClicks<2){
+reset()
+
+function player1Wins(){
+    return winsByRow(PLAYER1)||winsByDiagonal(PLAYER1)||winsByColumn(PLAYER1)
+}
+
+function player2Wins(){
+    return winsByRow(PLAYER2)||winsByDiagonal(PLAYER2)||winsByColumn(PLAYER2)
+}
+
+function winsByRow(player){
+    return grid.some((currentValue) => currentValue.every((value)=> value==player))
+}
+
+function winsByColumn(player){
+    return grid[0][0]==player&&grid[1][0]==player&&grid[2][0]==player||grid[0][1]==player&&grid[1][1]==player&&grid[2][1]==player||grid[0][2]==player&&grid[1][2]==player&&grid[2][2]==player
+}
+
+function winsByDiagonal(player){
+    return grid[0][0]==player&&grid[1][1]==player&&grid[2][2]==player||grid[0][2]==player&&grid[1][1]==player&&grid[2][0]==player
+}
+
+function reset(){
+    player1Turn = true;
+    grid = [[],[],[]];
+
+    square.forEach(element => {
+        element.textContent = ''
+        let numOfClicks = 0
+        switch(element.id){
+            case '1':
+            case '2':
+            case '3':
+                grid[0].push(undefined)
+                break;
+            case '4':
+            case '5':
+            case '6':
+                grid[1].push(undefined)
+                break;
+            default:
+                grid[2].push(undefined)
+                break;
+        }
+        element.addEventListener('click', (e) => {
+            numOfClicks++;
+            if(numOfClicks<=1&&(!player1Wins()&&!player2Wins())){
                 assignContent(element)
-            } else{
+            }
+            if(player1Wins()){
+                console.log('gano jugador 1')
+            }
+            else if(player2Wins()){
+                console.log('gano jugador 2')
+            }
+            else{
                 return false
             }
+        })
     })
-});
+}
